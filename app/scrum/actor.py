2b34829c8243f029e120c237175728302c13b6a6
neylin <neylinadriana91@gmail.com>
2015-05-13 14:58:00 -0430

    Nuevo model y modificaciones el actor

--- app/scrum/actor.py
+++ app/scrum/actor.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
+from model import *
 from flask import request, session, Blueprint, json
 
 actor = Blueprint('actor', __name__)
 
 
 @actor.route('/actor/ACrearActor', methods=['POST'])
-def ACrearActor():
+def ACrearActor(self, id_actores, nombre_actores):
     #POST/PUT parameters
     params = request.get_json()
     results = [{'label':'/VProducto', 'msg':['Actor creado']}, {'label':'/VCrearActor', 'msg':['Error al crear actor']}, ]
@@ -14,6 +15,11 @@ def ACrearActor():
 
     idPila = 1
     res['label'] = res['label'] + '/' + str(idPila)
+
+    # -- Insertar actor -- #
+    actor = model.Actores(id_actores, nombre_actores)
+    session.add(actor)
+    session.commit()
 
     #Action code ends here
     if "actor" in res:
@@ -26,7 +32,7 @@ def ACrearActor():
 
 
 @actor.route('/actor/AModifActor', methods=['POST'])
-def AModifActor():
+def AModifActor(self, id_actores, nombre_actores):
     #POST/PUT parameters
     params = request.get_json()
     results = [{'label':'/VProducto', 'msg':['Actor actualizado']}, {'label':'/VActor', 'msg':['Error al modificar actor']}, ]
@@ -35,6 +41,21 @@ def AModifActor():
 
     idPila = 1
     res['label'] = res['label'] + '/' + str(idPila)
+
+    # -- Modificar Rol -- #
+    def modify_id_actores(self, id_actores, new):
+        if (id_actores != None):
+            session.query(model.Actores).filter(model.Actores.id_actores == id_actores).update({'id_actores':(new)})
+            session.commit()
+            return True
+        return None
+    
+    def modify_nombre_actores(self, nombre_actores, new):
+        if (nombre_actores != None):
+            session.query(model.Actores).filter(model.Actores.nombre_actores == nombre_actores).update({'nombre_actores':(new)})
+            session.commit()
+            return True
+        return None
 
     #Action code ends here
     if "actor" in res:

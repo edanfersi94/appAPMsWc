9d7437f7a67fc1542811fbc750bc41c2692dcb8c
neylin <neylinadriana91@gmail.com>
2015-05-14 14:42:52 -0430

    Se agrego contador a los id y funciones de actor.py

--- app/scrum/actor.py
+++ app/scrum/actor.py
@@ -1,25 +1,25 @@
 # -*- coding: utf-8 -*-
-from model import *
+#from base import *
 from flask import request, session, Blueprint, json
 
 actor = Blueprint('actor', __name__)
 
 
 @actor.route('/actor/ACrearActor', methods=['POST'])
-def ACrearActor(self, id_actores, nombre_actores):
+def ACrearActor(self, nombre_actores):
     #POST/PUT parameters
     params = request.get_json()
     results = [{'label':'/VProducto', 'msg':['Actor creado']}, {'label':'/VCrearActor', 'msg':['Error al crear actor']}, ]
     res = results[0]
     #Action code goes here, res should be a list with a label and a message
 
+    # -- Insertar actor -- #
+    actor = base.Actores(nombre_actores)
+    db.session.add(actor)
+    db.session.commit()
+
     idPila = 1
     res['label'] = res['label'] + '/' + str(idPila)
-
-    # -- Insertar actor -- #
-    actor = model.Actores(id_actores, nombre_actores)
-    session.add(actor)
-    session.commit()
 
     #Action code ends here
     if "actor" in res:
@@ -32,7 +32,7 @@ def ACrearActor(self, id_actores, nombre_actores):
 
 
 @actor.route('/actor/AModifActor', methods=['POST'])
-def AModifActor(self, id_actores, nombre_actores):
+def AModifActor(self, id_actores, nombre_nuevo):
     #POST/PUT parameters
     params = request.get_json()
     results = [{'label':'/VProducto', 'msg':['Actor actualizado']}, {'label':'/VActor', 'msg':['Error al modificar actor']}, ]
@@ -42,20 +42,14 @@ def AModifActor(self, id_actores, nombre_actores):
     idPila = 1
     res['label'] = res['label'] + '/' + str(idPila)
 
-    # -- Modificar Rol -- #
-    def modify_id_actores(self, id_actores, new):
-        if (id_actores != None):
-            session.query(model.Actores).filter(model.Actores.id_actores == id_actores).update({'id_actores':(new)})
-            session.commit()
-            return True
-        return None
-    
-    def modify_nombre_actores(self, nombre_actores, new):
-        if (nombre_actores != None):
-            session.query(model.Actores).filter(model.Actores.nombre_actores == nombre_actores).update({'nombre_actores':(new)})
-            session.commit()
-            return True
-        return None
+    # -- Modificar Actor -- #
+    if (id_actores == None):
+        return False
+    else:
+        db.session.query(base.Actores).filter(base.Actores.id_actores == id_actores).update({'nombre_actores':(nombre_nuevo)})
+        db.session.commit()
+        return True
+
 
     #Action code ends here
     if "actor" in res:
@@ -97,6 +91,7 @@ def VCrearActor():
 
 
 #Use case code starts here
+
 
 
 #Use case code ends here

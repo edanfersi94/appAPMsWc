b9cd967c6b3c5cf67fed6dbc3c7c1f3e9f330fa9
oggs22 <oggs_22@hotmail.com>
2015-05-17 13:17:05 -0430

    Se añadieron 29 casos

--- app/scrum/testing/testobjetivo.py
+++ app/scrum/testing/testobjetivo.py
@@ -218,20 +218,93 @@ class TestObjetivo(unittest.TestCase):
     
     # FUNCION MODIFICAR
     
+    ### CASOS VALIDOS( Casos Esquinas )
+    # Test 20: El id del objetivo a modificar existe en la base de datos y su valor es
+    #          igual a 1.
     
-    
-    
-    
-    
-    
-    
-     
+    def test_20_modify_ObjValid(self):
+        tempObj = clsObjetivo()
+        idObjetivo = 1
+        newDescripObjetivo = 'z'
+        result = tempObjetivo.modify_Objetivo( idObjetivo, newDescripObjetivo )
+        self.assertTrue( result ) 
         
-        
+    # Test 21: El id del objetivo a modificar existe en la base de datos y su valor es
+    #          igual a 1. La nueva descripción es de longitud igual a 500.
     
-        
-    ## 
+    def test_21_modify_ObjValid(self):
+        tempObj = clsObjetivo()
+        idObjetivo = 1
+        newDescripObjetivo = 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec sodales sagittis magna. Sed consequat, leo eget bibendum sodales, augue velit cursus nunc, quis gravida magna mi a libero. Fusce vulputate eleifend sapien. Vestibulum purus quam, scelerisque ut, mollis sed, nonummy id, metus. Nullam accumsan lorem in dui. Cras ultricies mi eu turpis hendrerit fringilla. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; In ac dui quis mi consectetuer lacinia. Nam pretium turpis et arcu. Duis arcu tortor, suscipit eget, imperdiet nec, imperdiet iaculis, ipsum. Sed aliquam ultrices mauris. Integer ante arcu, accumsan a, consectetuer eget, posuere ut, mauris. Praesent adipiscing. Phasellus ullamcorper ipsum rutrum nunc. Nunc nonummy metus. Vestibulum volutpat pretium libero. Cras id dui. Aenean ut eros et nisl sagittis vestibulum. Nullam nulla eros, ultricies sit amet, nonummy id, imperdiet feugiat, pede. Sed lectus. Donec mollis hendrerit risus. Phasellus nec sem in justo pellentesque facilisis. Etiam imperdiet imperdiet orci. Nunc nec neque. Phasellus leo dolor, tempus non, auctor et, hendrerit quis, nisi. Curabitur ligula sapien, tincidunt non, euismod vitae, posuere imperdiet, leo. Maecenas malesuada. Praesent congue erat at massa. Sed cursus turpis vitae tortor. Donec posuere vulputate arcu. Phasellus accumsan cursus velit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Sed aliquam, nisi quis porttitor congue, elit erat euismod orci, ac placerat dolor lectus quis orci. Phasellus consectetuer vestibulum elit. Aenean tellus metus, bibendum sed, posuere ac, mattis non, nunc. Vestibulum fringilla pede sit amet augue. In turpis. Pellentesque posuere. Praesent turpis. Aenean posuere, tortor sed cursus feugiat, nunc augue blandit nunc, eu sollicitudin urna dolor sagittis lacus. Donec elit libero, sodales nec, volutpat a, suscipit non, turpis. Nullam sagittis. Suspendisse pulvinar, augue ac venenatis condimentum, sem libero volutpat nibh, nec pellentesque velit pede quis nunc. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Fusce id purus. Ut varius tincidunt libero. Phasellus dolor. Maecenas vestibulum mollis'
+        result = tempObjetivo.modify_Objetivo( idObjetivo, newDescripObjetivo )
+        self.assertTrue( result ) 
     
+    ### CASOS INVALIDOS( Casos Malicia )
+    # Test 22: El id dado del objetivo a modificar es un string.
+    def test_22_modify_ObjInvalid(self):        
+        tempObj = clsObjetivo()
+        idObjetivo = '1'
+        newDescripObjetivo = 'Abss'
+        result = tempObjetivo.modify_Objetivo( idObjetivo, newDescripObjetivo )
+        self.assertFalse( result )   
+        
+    # Test 23: El id dado del objea modificar es un numero negativo.    
+    def test_23_modify_ObjInvalid(self):        
+        tempObj = clsObjetivo()
+        idObjetivo = -1
+        newDescripObjetivo = 'accion de prueba'
+        result = tempObjetivo.modify_Objetivo( idObjetivo, newDescripObjetivo )
+        self.assertFalse( result )   
+
+    # Test 24: El id dado del objetivo a modificar es un float.
+    def test_24_modify_ObjInvalid(self):        
+        tempObj = clsObjetivo()
+        idObjetivo = 1.0
+        newDescripObjetivo = 'accion de prueba'
+        result = tempObjetivo.modify_Objetivo( idObjetivo, newDescripObjetivo )
+        self.assertFalse( result )   
+        
+    # Test 25: El id dado del objetivo a modificar es None.         
+    def test_25_modify_ObjInvalid(self):        
+        tempObj = clsObjetivo()
+        idObjetivo = None
+        newDescripObjetivo = 'accion de prueba'
+        result = tempObjetivo.modify_Objetivo( idObjetivo, newDescripObjetivo )
+        self.assertFalse( result )  
+    
+    # Test 26: La nueva descripción para el objetivo a modificar es un string vacio.
+    def test_26_modify_ObjInvalid(self):        
+        tempObj = clsObjetivo()
+        idObjetivo = 1
+        newDescripObjetivo = ''
+        result = tempObjetivo.modify_Objetivo( idObjetivo, newDescripObjetivo )
+        self.assertFalse( result ) 
+        
+    # Test 27: La nueva descripción para el objetivo  a modificar es de longitud 501.    
+    def test_27_modify_ObjInvalid(self):        
+        tempObj = clsObjetivo()
+        idObjetivo = 1
+        newDescripObjetivo = 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec sodales sagittis magna. Sed consequat, leo eget bibendum sodales, augue velit cursus nunc, quis gravida magna mi a libero. Fusce vulputate eleifend sapien. Vestibulum purus quam, scelerisque ut, mollis sed, nonummy id, metus. Nullam accumsan lorem in dui. Cras ultricies mi eu turpis hendrerit fringilla. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; In ac dui quis mi consectetuer lacinia. Nam pretium turpis et arcu. Duis arcu tortor, suscipit eget, imperdiet nec, imperdiet iaculis, ipsum. Sed aliquam ultrices mauris. Integer ante arcu, accumsan a, consectetuer eget, posuere ut, mauris. Praesent adipiscing. Phasellus ullamcorper ipsum rutrum nunc. Nunc nonummy metus. Vestibulum volutpat pretium libero. Cras id dui. Aenean ut eros et nisl sagittis vestibulum. Nullam nulla eros, ultricies sit amet, nonummy id, imperdiet feugiat, pede. Sed lectus. Donec mollis hendrerit risus. Phasellus nec sem in justo pellentesque facilisis. Etiam imperdiet imperdiet orci. Nunc nec neque. Phasellus leo dolor, tempus non, auctor et, hendrerit quis, nisi. Curabitur ligula sapien, tincidunt non, euismod vitae, posuere imperdiet, leo. Maecenas malesuada. Praesent congue erat at massa. Sed cursus turpis vitae tortor. Donec posuere vulputate arcu. Phasellus accumsan cursus velit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Sed aliquam, nisi quis porttitor congue, elit erat euismod orci, ac placerat dolor lectus quis orci. Phasellus consectetuer vestibulum elit. Aenean tellus metus, bibendum sed, posuere ac, mattis non, nunc. Vestibulum fringilla pede sit amet augue. In turpis. Pellentesque posuere. Praesent turpis. Aenean posuere, tortor sed cursus feugiat, nunc augue blandit nunc, eu sollicitudin urna dolor sagittis lacus. Donec elit libero, sodales nec, volutpat a, suscipit non, turpis. Nullam sagittis. Suspendisse pulvinar, augue ac venenatis condimentum, sem libero volutpat nibh, nec pellentesque velit pede quis nunc. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Fusce id purus. Ut varius tincidunt libero. Phasellus dolor. Maecenas vestibulum mollis'
+        result = tempObjetivo.modify_Objetivo( idObjetivo, newDescripObjetivo )
+        self.assertFalse( result )   
+
+    # Test 28: La nueva descripción para el objetivo a modificar es un numero.
+    def test_28_modify_ObjInvalid(self):        
+        tempObj = clsObjetivo()
+        idObjetivo = 1
+        newDescripObjetivo = 12345
+        result = tempObjetivo.modify_Objetivo( idObjetivo, newDescripObjetivo )
+        self.assertFalse( result )   
+        
+    # Test 29: La nueva descripción para el objetivo a modificar es None. 
+    def test_29_modify_accionDescripNone(self):        
+        tempObj = clsObjetivo()
+        idObjetivo = 1
+        newDescripObjetivo = None
+        result = tempObjetivo.modify_Objetivo( idObjetivo, newDescripObjetivo )
+        self.assertFalse( result )  
+
+    #.-------------------------------------------------------------------.  
     
     
     
